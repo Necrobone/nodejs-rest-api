8a5227f69251a2f1415afdf6fd2ed1ca60cc09e9
Necrobone <necrobone@hotmail.com>
2022-06-07 20:44:31 +0200

    Allow CORS

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
