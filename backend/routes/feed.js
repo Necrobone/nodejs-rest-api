733519cef05685bf33f707d168832588c3479121
Necrobone <necrobone@hotmail.com>
2022-06-13 17:16:53 +0200

    Protecting get route

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -3,10 +3,11 @@ const express = require("express");
 const feedController = require("../controllers/feed");
 const { postFeedValidation } = require("../middlewares/postFeedValidation");
 const { putFeedValidation } = require("../middlewares/putFeedValidation");
+const isAuth = require("../middlewares/is-auth");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 router.post("/posts", postFeedValidation(), feedController.createPost);
 router.get("/posts/:id", feedController.getPost);
 router.put("/posts/:id", putFeedValidation(), feedController.updatePost);
