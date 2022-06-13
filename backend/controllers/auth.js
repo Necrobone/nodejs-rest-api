301ba7f1cf836ca276b28e9fadb39608b98bcc11
Necrobone <necrobone@hotmail.com>
2022-06-13 18:24:39 +0200

    Getting and updating the user status

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -88,3 +88,59 @@ exports.login = (request, response, next) => {
       next(error);
     });
 };
+
+exports.getStatus = (request, response, next) => {
+  User.findById(request.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User could not be found");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      response.status(200).json({
+        status: user.status,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+exports.updateStatus = (request, response, next) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const status = request.body.status;
+
+  User.findById(request.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User could not be found");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      user.status = status;
+      return user.save();
+    })
+    .then(() => {
+      response.status(200).json({
+        message: "Status Updated",
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
