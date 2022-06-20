60a2ecf713d6ff31b4849eb4def18deedc903845
Necrobone <necrobone@hotmail.com>
2022-06-20 19:40:54 +0200

    organizing tests

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -2,10 +2,21 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middlewares/is-auth');
 
-it('should throw an error if no authorization header is present', () => {
-   const request = {
-       get: () => null
-   };
+describe('Auth middleware', () => {
+    it('should throw an error if no authorization header is present', () => {
+        const request = {
+            get: () => null
+        };
 
-   expect(authMiddleware.bind(this, request, {}, () => {})).to.throw('Not authenticated');
-});
+        expect(authMiddleware.bind(this, request, {}, () => {})).to.throw('Not authenticated');
+    });
+
+    it('should throw an error if the authorization header is only one string', () => {
+        const request = {
+            get: () => 'xyz'
+        };
+
+        expect(authMiddleware.bind(this, request, {}, () => {})).to.throw('jwt must be provided');
+    });
+
+})
