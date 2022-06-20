8ee52b75951dd4b320fd06e8161cf1c3373fcf85
Necrobone <necrobone@hotmail.com>
2022-06-20 19:55:09 +0200

    Mocking with sinon

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -1,22 +1,54 @@
-const expect = require('chai').expect;
+const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 
-const authMiddleware = require('../middlewares/is-auth');
+const authMiddleware = require("../middlewares/is-auth");
 
-describe('Auth middleware', () => {
-    it('should throw an error if no authorization header is present', () => {
-        const request = {
-            get: () => null
-        };
+describe("Auth middleware", () => {
+  it("should throw an error if no authorization header is present", () => {
+    const request = {
+      get: () => null,
+    };
 
-        expect(authMiddleware.bind(this, request, {}, () => {})).to.throw('Not authenticated');
+    expect(authMiddleware.bind(this, request, {}, () => {})).to.throw(
+      "Not authenticated"
+    );
+  });
+
+  it("should throw an error if the authorization header is only one string", () => {
+    const request = {
+      get: () => "xyz",
+    };
+
+    expect(authMiddleware.bind(this, request, {}, () => {})).to.throw(
+      "jwt must be provided"
+    );
+  });
+
+  it("should throw an error if the token cannot be verified", () => {
+    const request = {
+      get: () => "Bearer xyz",
+    };
+
+    expect(authMiddleware.bind(this, request, {}, () => {})).to.throw();
+  });
+
+  it("should throw an error if the token cannot be verified", () => {
+    const request = {
+      get: () => "Bearer xyz",
+    };
+
+    sinon.stub(jwt, "verify");
+
+    jwt.verify.returns({
+      userId: "abc",
     });
 
-    it('should throw an error if the authorization header is only one string', () => {
-        const request = {
-            get: () => 'xyz'
-        };
+    authMiddleware(request, {}, () => {});
 
-        expect(authMiddleware.bind(this, request, {}, () => {})).to.throw('jwt must be provided');
-    });
+    expect(request).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
 
-})
+    jwt.verify.restore();
+  });
+});
